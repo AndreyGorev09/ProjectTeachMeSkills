6a423bd0f0b1d9974fea7f1788499a583be17845
AndreiGorev <and.goreff2010@gmail.com>
2021-09-15 15:02:53 +0300

    modif view

--- superproject/task4/views.py
+++ superproject/task4/views.py
@@ -15,19 +15,19 @@ def view(request: HttpRequest):
 
     name = request.headers.get("x-user")
     if not name: return HttpResponse(status=422)
-
-    cell = json.loads(request.body)
-    obj, _created = Numbers.objects.get_or_create(name=name)
-    if type(cell) is int:
-        if not -100 <= cell <= 100:
-            return HttpResponse("Enter a number from -100 to 100", status=422)
-        obj.n += cell
-        obj.save()
-        return HttpResponse({obj.n})
-    elif cell == 'stop':
-        return HttpResponse({obj.n})
-    else:
-        return HttpResponse("""Your "body" should be a "number" or the word "stop" """, status=422)
+    try:
+        cell = json.loads(request.body)
+        obj, _created = Numbers.objects.get_or_create(name=name)
+        if type(cell) is int:
+            if not -100 <= cell <= 100:
+                return HttpResponse("Enter a number from -100 to 100", status=422)
+            obj.n += cell
+            obj.save()
+            return HttpResponse({obj.n})
+        elif cell == 'stop':
+            return HttpResponse({obj.n})
+    except ValueError as err:
+            return HttpResponse(f"""Your "body" should be a "number" or the word "stop", {err} """, status=422)
 
 
 
